8d73bc2b66eae96dcb48f43ac93e05edf355925e
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2025-02-18 19:19:17 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroPrograma/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PrimeiroPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe seu nome: ");
+            string nome = Console.ReadLine()!;
+            Console.WriteLine("\n");
+            Console.Write("Informe a Nota 1: ");
+            //float nota1 = Convert.ToSingle(Console.ReadLine()!);
+            float nota1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+            Console.Write("Informe a Nota 2: ");
+            float nota2 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+            Console.Write("Informe a Nota 3: ");
+            float nota3 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+
+            float soma = nota1 + nota2 + nota3;
+            //decimal media = Convert.ToInt64(soma / 3); "Para tranformar decimal em 64 bit"
+            double media = (soma / 3);
+
+            Console.WriteLine($"A média de {nome} é {media}");
+
+        }
+    }
+}
---
+++ StatusAluno/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace StatusAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = 0;
+            double nota2 = 0;
+            string mensagem = "";
+            double media = 0;
+
+            Console.WriteLine("Informe a 1a nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n");
+            Console.WriteLine("Informe a 2a nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n");
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                mensagem = "Aprovado";
+            }
+            else if (media >= 5 && media < 7)
+            {
+                mensagem = "Recuperação";
+            }
+            else
+            {
+                mensagem = "Reprovado";
+            }
+
+            Console.WriteLine($"O Status do Aluno de Média: {media} é {mensagem}");
+        }
+    }
+}
