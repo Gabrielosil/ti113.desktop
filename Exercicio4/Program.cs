76b6425e806a750837d4342e2f7f692da737f0dc
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2025-02-20 19:15:57 -0300

    .

--- Exercicio4/Program.cs
+++ Exercicio4/Program.cs
@@ -4,8 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
+            Console.Write("Digite o primeiro número: ");
+            if (double.TryParse(Console.ReadLine(), out double numero1))
+            {
+                Console.WriteLine("\n");
+                Console.Write("Digite o segundo número: ");
+                if (double.TryParse(Console.ReadLine(), out double numero2))
+                {
+                    Console.Write("Digite o terceiro número: ");
+                    if (double.TryParse(Console.ReadLine(), out double numero3))
+                    {
+                        double maior = Math.Max(numero1, Math.Max(numero2, numero3));
+                        Console.WriteLine($"O maior número é: {maior}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada inválida! Digite um número válido.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida! Digite um número válido.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida! Digite um número válido.");
+            }
         }
     }
 }
